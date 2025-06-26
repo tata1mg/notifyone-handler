e5b82609f1777b22c46863687b074284d168291a
Akash Bhat <akashbhat@FVFJ620QQ6L4.local>
2025-06-26 13:03:59 +0530

    added whatsapp constants

--- app/constants/whatsapp.py
+++ app/constants/whatsapp.py
@@ -1,12 +1,11 @@
 from enum import Enum
 
+class Whatsapp:
+    CONTENT_COLUMNS=['id','event_id','name']
+    NAME =  'name'
 
-class WhatsAppEventStatus(str, Enum):
-    SENT = "SENT"
-    DELIVERED = "DELIVERED"
-    READ = "READ"
-    FAILED = "FAILED"
-    UNKNOWN = "UNKNOWN"
 
-    def __str__(self) -> str:
-        return self.value
+class VariableMappingKeys(Enum):
+    BODY = "body"
+    HEADER = "header"
+    BUTTON = "button"
