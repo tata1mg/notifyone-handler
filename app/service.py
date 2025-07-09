4968d420288ff4c82d01aa51ea6d59f03b297c7f
Akash Bhat <akashbhat@FVFJ620QQ6L4.local>
2025-07-09 13:09:31 +0530

    public repo changes

--- app/service.py
+++ app/service.py
@@ -1,9 +1,16 @@
+from torpedo import Host
+
+from app.initialize import Initialize
 from app.listeners import listeners
 from app.routes import blueprint_group
-from torpedo import Torpedo
 
-torpedo = Torpedo(blueprint_group, listeners)
-app = torpedo.create_app()
 if __name__ == "__main__":
-    torpedo.run()
+    # register listeners
+    Host._listeners = listeners
 
+    # register combined blueprint group here.
+    # these blueprints are defined in the routes directory and has to be
+    # collected in init file otherwise route will end up with 404 error.
+    Initialize.initialize_service_startup_dependencies()
+    Host._blueprint_group = blueprint_group
+    Host.run()
